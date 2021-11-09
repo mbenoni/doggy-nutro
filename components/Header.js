1e5b49522abfc3938920b5ce9e4f6d80c75bf802
Magnus Benoni <magnus@benoni.no>
2021-11-09 14:19:15 +0100

    Add Header component

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="header-content container">
+        <img className="logo" src="/logo/logo_hz.svg" alt="Doggy Nutro logo" />
+        <a href="#">Til nettbutikken</a>
+      </div>
+    </header>
+  );
+}
