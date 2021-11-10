3173d29dec0359ea2530c3f29bc40de9642b45dc
Magnus Benoni <magnus@benoni.no>
2021-11-10 23:58:39 +0100

    Fetch data from API

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,36 @@
+import { useState } from 'react';
 import Header from '../components/Header';
 
-export default function Home() {
+export async function getStaticProps() {
+  const res = await fetch('https://api.thedogapi.com/v1/breeds', {
+    headers: {
+      'x-api-key': process.env.API_KEY
+    }
+  });
+  const dogBreeds = await res.json();
+
+  return {
+    props: {
+      dogBreeds
+    }
+  };
+}
+
+export default function Home({ dogBreeds }) {
   return (
     <>
       <Header />
 
       <main>
         <div className="container">
-          <h1>Ernæringsveilederen</h1>
-          <p>Visste du hvor viktig det er å velge riktig fôr til din hund? Prøv
-            vår enkle ernæringsveileder som hjelper deg å velge riktig ernæring
-            til din firbeinte venn. Svar på noen enkle spørsmål og få veiledning
-            til hvilket fôr du bør velge. Gi hunden din det aller beste den kan
-            få!</p>
+        <h1>Ernæringsveilederen</h1>
+        <p>Visste du hvor viktig det er å velge riktig fôr til din hund? Prøv
+          vår enkle ernæringsveileder som hjelper deg å velge riktig ernæring
+          til din firbeinte venn. Svar på noen enkle spørsmål og få veiledning
+          til hvilket fôr du bør velge. Gi hunden din det aller beste den kan
+          få!</p>
         </div>
       </main>
     </>
   );
 }
-
