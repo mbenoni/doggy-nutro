20930cfa91426588604825f14d14f2d806256874
Magnus Benoni <magnus@benoni.no>
2021-11-11 00:10:08 +0100

    Fix indentation

--- pages/index.js
+++ pages/index.js
@@ -23,12 +23,12 @@ export default function Home({ dogBreeds }) {
 
       <main>
         <div className="container">
-        <h1>Ernæringsveilederen</h1>
-        <p>Visste du hvor viktig det er å velge riktig fôr til din hund? Prøv
-          vår enkle ernæringsveileder som hjelper deg å velge riktig ernæring
-          til din firbeinte venn. Svar på noen enkle spørsmål og få veiledning
-          til hvilket fôr du bør velge. Gi hunden din det aller beste den kan
-          få!</p>
+          <h1>Ernæringsveilederen</h1>
+          <p>Visste du hvor viktig det er å velge riktig fôr til din hund? Prøv
+            vår enkle ernæringsveileder som hjelper deg å velge riktig ernæring
+            til din firbeinte venn. Svar på noen enkle spørsmål og få veiledning
+            til hvilket fôr du bør velge. Gi hunden din det aller beste den kan
+            få!</p>
         </div>
       </main>
     </>
