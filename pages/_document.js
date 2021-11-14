cc9f0d0d44876b329d5eacd1d1797a3dd78bfde5
Magnus Benoni <magnus@benoni.no>
2021-11-14 23:23:38 +0100

    Use favicons

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,27 @@ class MyDocument extends Document {
       <Html lang="nb">
         <Head>
           <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="manifest"
+            href="/favicon/site.webmanifest"
+          />
+          <link
             rel="preconnect"
             href="https://fonts.googleapis.com"
           />
