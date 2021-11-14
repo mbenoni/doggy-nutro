254a082f7a43e2f6b9509dd00b4dd3883d31f4c7
Magnus Benoni <magnus@benoni.no>
2021-11-14 22:18:33 +0100

    Add explicit width and height to logo

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,13 @@ export default function Header() {
   return (
     <header>
       <div className="header-content container">
-        <img className="logo" src="/logo/logo_hz.svg" alt="Doggy Nutro logo" />
+        <img
+          className="logo"
+          src="/logo/logo_hz.svg"
+          alt="Doggy Nutro logo"
+          width="267"
+          height="27"
+        />
         <a href="#">Til nettbutikken</a>
       </div>
     </header>
